18d29ce4f606d0742bca163b3beb4fb4be41de53
Your Name <you@example.com>
2023-10-03 17:26:38 -0300

    atualizacao

--- src/screens/FetchScreen.jsx
+++ src/screens/FetchScreen.jsx
@@ -5,19 +5,27 @@ export default function FetchScreen() {
 
   async function trazerLista() {
     const url = "https://api.tvmaze.com/shows";
-
     // eu solicito que o servidor me envie os dados
     const consulta = await fetch(url);
     // eu converto os dados recebidos em um objeto javascript JSON
     const dados = await consulta.json();
-
-    console.log(dados);
+    //console.log(dados);
+    // depois que dados foi convertido em objeto JSON
+    // eu posso manipular os dados e realizar o loop
+    // para cada item do array de dados
+    dados.forEach(
+      // crio uma função anonima que recebe o item do array
+      (filme) => {
+        // para cada filme eu exibo o nome
+        console.log(filme.name);
+      }
+    );
   }
 
   return (
     <div className="container">
       <h1 className="text-center">Exercício Fetch</h1>
-      <button onClick={() => trazerLista}></button>
+      <button onClick={trazerLista}>Trazer lista</button>
     </div>
   );
 }
