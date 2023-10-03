89022ee9516783105671b368c085857d76d5ffac
Your Name <you@example.com>
2023-10-03 16:52:06 -0300

    aula 03-10

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function HomeScreen() {
   return (
-    <div>
+    <div className="container">
       <h1>Home Screen Happy ❤️❤️</h1>
       <Outlet />
     </div>
